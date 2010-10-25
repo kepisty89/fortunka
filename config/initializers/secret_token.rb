96cce57e7097a84ce5d97b1d77148afbcb3283cc
Łukasz <kepisty89@gmail.com>
2010-10-25 14:30:19 +0200

    prepared a template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka2::Application.config.secret_token = '625cbd337a7748f119e86c6faa49df70f42f529bd7c9d2035b94052d917e35524795e6e0cf72d2233a52c0880a299160e8f1005a0ed381df6d83bf828502a632'
